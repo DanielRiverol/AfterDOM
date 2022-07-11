3cd60128b65d336a5ac24efc7431b6f9e4e4809d
Daniel Riverol <34141421+DanielRiverol@users.noreply.github.com>
2022-07-11 20:12:17 -0300

    Archivos de muestra

---
+++ afterDOMLive/js/main.js
@@ -0,0 +1,9 @@
+const form = document.querySelector("form");
+
+const inputNombre = document.querySelector("#nombre"),
+  inputDescripcion = document.querySelector("#descripcion"),
+  inputPrecio = document.querySelector("#precio"),
+  inputImg = document.querySelector("#img"),
+  btnGuardar = document.querySelector("#btnGuardar"),
+  contenedor = document.querySelector("#contenedor");
+
